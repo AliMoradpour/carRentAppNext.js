9609d57a0e4be404095bc64738e2fb19a43551e1
AliMoradpour <mr.mrdp@yahoo.com>
2023-08-19 09:55:47 +0330

    complete project

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,19 +1,23 @@
-"use client"
+"use client";
 
 import Image from "next/image";
-import { CustomButton } from ".";
+
+import { CustomButton } from "@components";
 
 const Hero = () => {
+  const handleScroll = () => {
+    const nextSection = document.getElementById("discover");
 
-const handleScroll = ()=>{
-
-}
+    if (nextSection) {
+      nextSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
 
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
         <h1 className="hero__title">
-          Find, Book, or rent a car — quickly and easily!
+          Find, book, rent a car—quick and super easy!
         </h1>
 
         <p className="hero__subtitle">
@@ -21,17 +25,18 @@ const handleScroll = ()=>{
           process.
         </p>
 
-        <CustomButton 
-        title="Explore Cars"
-        containerStyles="bg-primary-blue text-white rounded-full mt-10"
-        handleClick={handleScroll}
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
         />
       </div>
       <div className="hero__image-container">
         <div className="hero__image">
-            <Image src="/hero.png" alt="hero" fill className="object-contain" />
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
         </div>
-            <div className="hero__image-overlay"/>
+
+        <div className="hero__image-overlay" />
       </div>
     </div>
   );
